e2cc999a05946947af46497bac8926b84904bdd3
isabelhurley <iah.19@dartmouth.edu>
2018-05-14 21:52:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,23 +5,9 @@ new Vue({
   // this is a little diferent from how they're imported and used in React
   el: '#main-app',
   data: {
-    health: 100,
-    ended: false,
+    
   },
   methods: {
-    punch() {
-      console.log("punch");
-      this.health -= 10;
-      if (this.health <= 0) {
-        this.ended = true;
-      }
-    },
-    restart() {
-      this.health = 100;
-      this.ended = false;
-    },
-  },
-  computed: {
-
+    
   },
 });
